56fc7761b40f8a93f5e7e7a023c4071dd9f5f980
thienle <thienkyo@gmail.com>
2016-10-09 17:09:49 +0700

    first commit : testing

--- app/src/main/java/com/rmc/thienle/jedi2/MainActivity.java
+++ app/src/main/java/com/rmc/thienle/jedi2/MainActivity.java
@@ -42,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setSubtitle(R.string.status_not_connect);
+
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -150,11 +155,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "OUTLET 1";
                 case 1:
-                    return "SECTION 2";
+                    return "OUTLET 2";
                 case 2:
-                    return "SECTION 3";
+                    return "OUTLET 3";
             }
             return null;
         }
