43b5b778e736c5d8813d7aa2d404c28daab4835e
thienkyo <thienkyo@gmail.com>
2016-11-14 18:30:05 +0700

    update: DBHelper

--- app/src/main/java/com/rmc/thienle/jedi2/DBHelper.java
+++ app/src/main/java/com/rmc/thienle/jedi2/DBHelper.java
@@ -201,7 +201,7 @@ public class DBHelper extends SQLiteOpenHelper {
         return db.update(Relay.TABLE_NAME, contentValues, Relay.COLUMN_NAME_RELAY_ID + " = ? ", new String[] { String.valueOf(relay_id) }) > 0;
     }
 
-    public ArrayList<Entry> getAllEntryBySwitch()
+    public ArrayList<Entry> getAllEntryBySwitch()/// test test
     {
         ArrayList<Entry> array_list = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
